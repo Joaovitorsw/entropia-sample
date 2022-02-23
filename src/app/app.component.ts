e5e8c81796238138d66ee03a18e54e2944a7c5db
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-22 23:00:47 -0300

    ✨ feat: fav icon, change style to scss and includ schematics configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'entropia-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'entropia-sample';
-}
+export class AppComponent {}
